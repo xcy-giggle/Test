d3724f77b79cf7062fc735fef46128d84d3a9c3e
xcy-giggle <3396002414@qq.com>
2024-12-06 11:51:54 +0800

    李四的修改

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("减法结果: " + calculator.subtract(10, 5));
         System.out.println("乘法结果: " + calculator.multiply(10, 5));
         System.out.println("除法结果: " + calculator.divide(10, 5));
+        System.out.println("加法结果: " + calculator.add(20, 5));
     }
 }
 
